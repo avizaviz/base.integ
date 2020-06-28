355adc9e7a8cf71e1b16859794ea047cf6c1df63
Alon4455 <60478058+Alon4455@users.noreply.github.com>
2020-06-28 03:11:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.base = class base {
   async startBrowserAndPage(puppeteer) {
     this.browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       devtools: true,
       userDataDir: "./",
       args: ["--no-sandbox"],
