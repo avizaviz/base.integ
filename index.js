48d5346e01f6f42eca50417ced4f42598dce6694
Alon4455 <60478058+Alon4455@users.noreply.github.com>
2020-08-05 14:22:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.base = class base {
       userDataDir: "./", 
       ignoreHTTPSErrors: true,
       acceptInsecureCerts: true,
-      args: ['--proxy-bypass-list=*', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-first-run', '--no-sandbox', '--no-zygote', '--single-process', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService']
+      args: ['--proxy-bypass-list=*', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-first-run', '--no-sandbox', '--no-zygote', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService']
      /// args: ["--no-sandbox"],
     
      
